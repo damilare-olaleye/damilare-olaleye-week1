32ff82f06e3521d61a8d06ad854ed66cc5bc47d3
Damilare Olaleye <damilare.olaleye@cyatmr.com>
2021-10-19 15:13:50 -0500

    breakout rooms

---
+++ Training/week1_workspace/methods-and-parameters/src/com/revature/model/Animal.java
@@ -0,0 +1,55 @@
+package com.revature.model;
+
+public class Animal {
+
+	public String species;
+	public String name;
+	public int age;
+	public int energyLevel;
+	
+	public Animal(String species, String name, int age, int energyLevel) {
+		this.species = species;
+		this.name = name;
+		this.age = age;
+		this.energyLevel = energyLevel;
+				
+	}
+	
+	/*
+	 * Methods : A method is a block of code that can be executed whenever it is invoked within another method
+	 * 
+	 * Methods that do not have the "static" modifier are known as "instance methods"
+	 * 
+	 * Instance methods are methods that belong to a particular object and can modify and access the instance properties of that object.
+	 * 
+	 */
+	
+	public void play() {
+		System.out.println(this.name + " is playing");
+		this.energyLevel = this.energyLevel - 10;
+	}
+	
+	// Method overloading: If you have 2 or more methods with the same name defined in the same class,
+	//but with different numbers of parameters / type of parameter.
+	// For all intents and purposes, from the perspective of the JVM when the program is being ran, overloaded methods are NOT related in any way.
+	// Overloaded methods are two totally different methods from each other
+	public void play(String game) {
+		System.out.println(this.name + "is playing" + game + "!");
+		this.energyLevel = this.energyLevel - 10;
+	}
+
+	
+	public void play(int energyLevelDecrease) {
+		System.out.println(this.name + " is playing");
+		this.energyLevel = this.energyLevel -  energyLevelDecrease;
+	}
+	
+	
+	public void eat(Food food) {
+		System.out.println(this.name + " is eating" + food.name);
+		this.energyLevel = this.energyLevel + food.calories;
+	}
+
+}
+	
+
---
+++ Training/week1_workspace/methods-and-parameters/src/com/revature/model/Food.java
@@ -0,0 +1,14 @@
+package com.revature.model;
+
+public class Food {
+	
+	public int calories;
+	public String name;
+	
+	public Food(String name, int calories) {
+		this.name = name;
+		this.calories = calories;
+		
+	}
+
+}
