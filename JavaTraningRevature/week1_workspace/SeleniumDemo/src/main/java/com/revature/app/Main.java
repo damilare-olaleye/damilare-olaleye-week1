23185371a18a15eed86bfde709964d343a2ccbe9
Damilare Olaleye <damilare.olaleye@cyatmr.com>
2021-10-23 18:59:33 -0500

    little tweaks

--- JavaTraningRevature/week1_workspace/SeleniumDemo/src/main/java/com/revature/app/Main.java
+++ JavaTraningRevature/week1_workspace/SeleniumDemo/src/main/java/com/revature/app/Main.java
@@ -14,15 +14,15 @@ public class Main {
 		driver.get("http://localhost:8080");
 		
 		//to pause 
-		Thread.sleep(10000);
+		Thread.sleep(6000);
 		
 		// Locate the 2 input elements and button for adding numbers
 		WebElement addInput1 = driver.findElement(By.id("addNum1"));
 		WebElement addInput2 = driver.findElement(By.id("addNum2"));
 		WebElement addButton = driver.findElement(By.id("addButton"));
 		
-		addInput1.sendKeys("10.5");
-		addInput2.sendKeys("15.3");
+		addInput1.sendKeys("90.9");
+		addInput2.sendKeys("87.3");
 		addButton.click();
 		
 
@@ -40,7 +40,7 @@ public class Main {
 		// whenever we are done with the scripted tasks we will provide to Selenium 
 		// WebDriver. You should quit the driver.
 		
-//		driver.quit();
+		driver.quit();
 
 	}
 
